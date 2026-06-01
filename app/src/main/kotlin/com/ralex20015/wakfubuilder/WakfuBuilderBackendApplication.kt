df7a858b5d1d929b8c42023d407053690db90bbc
Alex Robles <lozanoeduardo8777@gmail.com>
2026-05-31 19:53:01 -0600

    Add Spring Boot application entry point and datasource configuration

---
+++ app/src/main/kotlin/com/ralex20015/wakfubuilder/WakfuBuilderBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.ralex20015.wakfubuilder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WakfuBuilderBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<WakfuBuilderBackendApplication>(*args)
+}
